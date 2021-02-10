2f0c6d6264281f455c579a1b65f0ce68bc11fd0d
YoonChang Kook <kkyc8909@naver.com>
2021-02-10 15:24:57 +0900

    :sparkles: Change to show what button is selected

---
+++ Assets/Scripts/ButtonSelected.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelected : MonoBehaviour
+{
+    private Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetText(string text)
+    {
+        this.text.text = text;
+    }
+}
